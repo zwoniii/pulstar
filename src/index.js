3d4dbd9c876399901395e8e8e9be8202a35af175
irazv <zwoniii@ya.ru>
2023-05-22 16:15:48 +0300

    shop slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './scss/style.scss';
 import './scss/_global.scss';
 import './scss/_reset.scss';
+
+new Swiper('.shop__slider', {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+});
