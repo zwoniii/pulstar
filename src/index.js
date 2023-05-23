a90278d7b6e2d59453515323f33c34636b5b7648
irazv <zwoniii@ya.ru>
2023-05-23 10:29:28 +0300

    shop slider css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './scss/style.scss';
 import './scss/_global.scss';
 import './scss/_reset.scss';
 
-new Swiper('.shop__slider', {
+const swiper = new Swiper('.shop__slider', {
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
