0843ad22033c6d27ae1a047e1ac2850b73dfd4fb
irazv <zwoniii@ya.ru>
2023-05-24 13:31:10 +0300

    adaptive header

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import './scss/style.scss';
 import './scss/_global.scss';
 import './scss/_reset.scss';
 
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+  console.log('1 menu', menu);
+  console.log('2', [...menu.classList]);
+  menu.classList.toggle('menu__list--active');
+  console.log('3', [...menu.classList]);
+});
+
+
 const swiper = new Swiper('.shop__slider', {
     pagination: {
         el: ".swiper-pagination",
